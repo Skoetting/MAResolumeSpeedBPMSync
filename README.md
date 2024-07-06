01dadd8dc57559c437fa4aa75a7e37313f13bf39
Skoetting <github_old@skoetting.net>
2024-07-06 17:05:10 +0200

    Initial Push

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# MAResolumeSpeedBPMSync
-GrandMA Resolume speed and BPM sync plugin
+# GrandMA2 Speed Master to Resolume Speed and BPM Sync
+This MA2 plugin is designed to sync the value of a GrandMA2 SpeedMaster to Resolume as Speedmaster and as BPM.
+The plugin was developed to always keep the Resolume visuals and effects (which rely on the Resolume BPM rather) in sync with the effects running on MA.
+
+
+## Installation:
+### 1) Copy files to the MA2 Plugin directory
+download and copy the files:
+- ResolumeSpeedAndBBPMSync.xml
+- ResolumeSpeedAndBBPMSync.lua
+
+from this repository into the plugin directory (e.g., on a USB-Drive: gma2/plugins/)
+
+### 2) Prepare fixtures and Resolume
+Follow the [Resolume Guide](https://resolume.com/support/en/dmx-shortcuts) to set up DMX Input and DMX shortcuts in Resolume.
+
+In MA2, add fixtures to your patch and map it according to the Resolume guide above:
+|Name|FixtureType|Resolume Function|Note|
+| -------- | ------- | -------- | ------- |
+|resolumeSpeedMaster|Generic Dimmer|Composition Speed ("S" Slider in header)||
+|resolumeBPM|Generic Dimmer 16 bit|Composition Tempocontroller Tempo(BPM)|check "16-bit" and specify range to be from 20 to 500|
+
+### 3) Import and configure plugin
+Right click in a empty plugin slot in the Plugin pool in MA2 -> import -> select ResolumeSpeedAndBBPMSync
+
+After importing, adjust the variables in the Configuration block.
+```
+-- Configuration block:
+local speedFader = 'SpecialMaster 3.2' -- if changed, reload plugin engine to get new Handle
+local resolumeBPM = '925' -- Change this to the appropriate fixture number
+local resolumeSpeedMaster = '921' -- Change this to the appropriate fixture number
+
+local speedexec = '115' -- Change this to the executor where the speed of the resolume fixtures should be stored. This executor will be created by the plugin
+local speedexecpage = '18' -- Specify the page for the executor
+local worldid = '42' -- Change this to set the world id, used to filter the output of the executor
+
+local resolumeBPMFrom = 20 -- Range set in Resolume DMX Shortcut
+local resolumeBPMTo = 500 -- Range set in Resolume DMX Shortcut
+
+local executiondelay = 0.5 -- interval to check for changed Fader Value in seconds
+```
+
+Enable "Execute on Load", press Save, exit the Plugin and call the plugin from the pool.
+
+### 4) Create trigger
+I like to have the possibility to enable/disable the sync; therefore, therefor the plugin is not starting automatically.
+To start the plugin, execute the command:
+```
+LUA "startResolumeSpeedSync()"
+```
+To stop the plugin, execute:
+```
+LUA "StopResolumeSpeedSync = true"
+```
+
+I just created a executor button with 2 empty cues and setting the above commands as cmd for the cues.
+
+
---
+++ ResolumeSpeedAndBBPMSync.lua
@@ -0,0 +1,96 @@
+-- Plugin: Sync BPM to resolume speedmaster and bpm
+-- Author: Simon Kötting
+
+
+-- Configuration block:
+local speedFader = 'SpecialMaster 3.2' -- if changed, reload plugin engine to get new Handle
+local resolumeBPM = '925' -- Change this to the appropriate fixture number
+local resolumeSpeedMaster = '921' -- Change this to the appropriate fixture number
+
+local speedexecpage = '18'
+local speedexec = '115' -- Change this to the executor where the speed of the resolume fixtures should be stored. This executor will be created by the plugin
+local worldid = '42' -- Change this to set the world id, used to filter the output of the executor
+
+local resolumeBPMFrom = 20 -- Range set in Resolume DMX Shortcut
+local resolumeBPMTo = 500 -- Range set in Resolume DMX Shortcut
+
+local executiondelay = 0.5 -- interval to check for changed Fader Value in seconds
+
+
+-- Code block:
+local lastBPM = -1 -- Variable to store the last BPM value
+local speedmasterhandle = gma.show.getobj.handle(speedFader)
+
+function doesNotExist(objectName)
+    local handle = gma.show.getobj.handle(objectName)
+    if handle ~= nil then
+      return false
+    else
+      return true
+    end
+end
+
+function initCheckExec()
+    if doesNotExist('Exec ' ..speedexecpage.. '.' ..speedexec) then -- create executor if not exists
+        gma.feedback('Not Exists')
+        gma.cmd('BlindEdit on')
+        if doesNotExist('Page ' ..speedexecpage ) then -- create page if not already exists
+            gma.cmd('Store Page ' ..speedexecpage)
+        end
+        gma.cmd('Store Exec ' ..speedexecpage.. '.' ..speedexec)
+        gma.cmd('Label Exec ' ..speedexecpage.. '.' ..speedexec.. ' "resolumespeedsync"')
+        if doesNotExist('World ' ..worldid) then -- create world if not exists
+            gma.cmd('Fixture ' ..resolumeBPM.. ' ' ..resolumeSpeedMaster)
+            gma.cmd('Store World ' ..worldid)
+            gma.cmd('Label World ' ..worldid.. ' "resolumespeed"')
+        end
+        gma.cmd('Assign World ' ..worldid.. ' Executor ' ..speedexecpage.. '.' ..speedexec)
+        gma.cmd('Assign World ' ..worldid.. ' Sequence "resolumespeedsync"')
+        gma.cmd('BlindEdit off')
+    end
+    gma.cmd('Go Executor ' ..speedexecpage.. '.' ..speedexec)
+end
+
+function setResolumeSpeedMaster(value)
+    gma.cmd('Fixture ' .. resolumeSpeedMaster .. ' At ' .. tostring(value))
+end
+
+function setResolumeBPM(value)
+    local resolumeBPMFixtureValue = -1
+    if value <= resolumeBPMFrom then
+        resolumeBPMFixtureValue = 0
+    elseif value >= resolumeBPMTo then
+        resolumeBPMFixtureValue = 255
+    else
+        resolumeBPMFixtureValue = (value - resolumeBPMFrom) * (100/(resolumeBPMTo - resolumeBPMFrom)) -- calculations as resolume Arena uses dmx value 0 as 20BPM - to work the range withing the DMX Shortcut Assignment have to be 20-225
+    end
+    gma.cmd('Fixture ' .. resolumeBPM .. ' At ' .. resolumeBPMFixtureValue)
+end
+
+function syncSpeedToResolume()
+    -- Get current BPM
+    local speedmasterState = gma.show.property.get(speedmasterhandle,1) -- percentage of fader (0-100)
+    speedmasterState = speedmasterState:gsub('%%','')
+    speedmasterState = tonumber(speedmasterState) 
+    local speedmasterbpm = 225 * (speedmasterState / 100)
+
+    -- Sync to Resolume if Value has changed
+    if speedmasterbpm ~= lastBPM then
+        gma.feedback('syncSpeedToResolume: Value changed')
+
+        setResolumeSpeedMaster(speedmasterState)
+        setResolumeBPM(speedmasterbpm)
+        gma.cmd('Store Executor ' ..speedexecpage.. '.' ..speedexec.. ' /o')
+
+        lastBPM = speedmasterbpm 
+    end
+end
+
+function startResolumeSpeedSync()
+    StopResolumeSpeedSync = false
+    initCheckExec()
+    while not StopResolumeSpeedSync do
+        syncSpeedToResolume()
+        gma.sleep(executiondelay)
+    end
+end
